5974b4822c6bef02000b8d8dfb76caf1673f6c12
Ashiq11 <ashiq.ac@outlook.com>
2022-09-27 07:10:36 -0600

    xUnit Test Project added for Application.UnitTests and Persistence.IntegrationTests

---
+++ GloboTicket.TicketManagement/TicketManagement.API.IntegrationTests/Controllers/CategoryControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using GloboTicket.TicketManagement.Api;
+using TicketManagement.API.IntegrationTests.Base;
+using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesList;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TicketManagement.API.IntegrationTests.Controllers
+{
+    //public class CategoryControllerTests : IClassFixture<CustomWebApplicationFactory<Program>>
+    public class CategoryControllerTests
+    {
+    //    private readonly CustomWebApplicationFactory<Program> _factory;
+
+    //    public CategoryControllerTests(CustomWebApplicationFactory<Program> factory)
+    //    {
+    //        _factory = factory;
+    //    }
+
+    //    [Fact]
+    //    public async Task ReturnsSuccessResult()
+    //    {
+    //        var client = _factory.GetAnonymousClient();
+
+    //        var response = await client.GetAsync("/api/category/all");
+
+    //        response.EnsureSuccessStatusCode();
+
+    //        var responseString = await response.Content.ReadAsStringAsync();
+
+    //        var result = JsonConvert.DeserializeObject<List<CategoryListVm>>(responseString);
+
+    //        Assert.IsType<List<CategoryListVm>>(result);
+    //        Assert.NotEmpty(result);
+    //    }
+    }
+}
